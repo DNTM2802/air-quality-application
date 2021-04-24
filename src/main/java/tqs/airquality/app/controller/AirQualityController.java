f9b83391774394cd12b00a5e2a1170e17e3e01db
Duarte Mortágua <duarte.ntm@ua.pt>
2021-04-24 01:59:40 +0100

    Added controller to recieve API calls, retrieve answers from services and render them to views.

--- src/main/java/tqs/airquality/app/controller/AirQualityController.java
+++ src/main/java/tqs/airquality/app/controller/AirQualityController.java
@@ -1,2 +1,56 @@
-package tqs.airquality.app.controller;public class AirQualityController {
+package tqs.airquality.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import tqs.airquality.app.repository.AirQualityRepository;
+import tqs.airquality.app.service.AirQualityService;
+
+@Controller
+@RequestMapping("/api")
+public class AirQualityController {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private AirQualityRepository airQualityRepository;
+
+    @Autowired
+    private AirQualityService airQualityService;
+
+    @GetMapping(value = "/air_quality/today")
+    public String getAirQualityOfTodayFromCoordinates(@RequestParam float coordLat, @RequestParam float coordLon, Model model) {
+
+        model.addAttribute("airQuality", airQualityService.getCurrentAirQuality(coordLat, coordLon));
+
+        return "current-air-quality";
+    }
+
+    @GetMapping(value = "/air_quality/forecast")
+    public String getAirQualityForecastFromCoordinates(@RequestParam float coordLat, @RequestParam float coordLon, Model model) {
+
+        model.addAttribute("airQualities", airQualityService.getForecastAirQuality(coordLat, coordLon));
+
+        return "forecast-air-quality";
+    }
+
+    @GetMapping(value = "/air_quality/historical")
+    public String getAirQualityHistoricalFromCoordinatesAndStartDateAndEndDate(
+            @RequestParam float coordLat,
+            @RequestParam float coordLon,
+            @RequestParam String startDate,
+            @RequestParam String endDate,
+            Model model) {
+
+        model.addAttribute("airQualities", airQualityService.getHistoricalAirQuality(coordLat, coordLon,startDate,endDate));
+
+        return "historical-air-quality";
+    }
 }
