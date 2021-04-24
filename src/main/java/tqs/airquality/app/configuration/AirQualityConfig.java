be3e5592a7956b33d77cf5256fb3ec66b1da58c4
Duarte Mortágua <duarte.ntm@ua.pt>
2021-04-24 01:58:00 +0100

    Added needed beans for JSON -> POJO mapping.

--- src/main/java/tqs/airquality/app/configuration/AirQualityConfig.java
+++ src/main/java/tqs/airquality/app/configuration/AirQualityConfig.java
@@ -1,2 +1,19 @@
-package tqs.airquality.app.configuration;public class AirQualityConfig {
+package tqs.airquality.app.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AirQualityConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
