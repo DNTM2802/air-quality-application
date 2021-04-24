f3f1f87258f69fcda0d30f629d4f1824cf134bef
Duarte Mortágua <duarte.ntm@ua.pt>
2021-04-24 02:01:41 +0100

    JPA interface for AirQuality.

--- src/main/java/tqs/airquality/app/repository/AirQualityRepository.java
+++ src/main/java/tqs/airquality/app/repository/AirQualityRepository.java
@@ -1,2 +1,10 @@
-package tqs.airquality.app.repository;public interface AirQualityRepository {
+package tqs.airquality.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tqs.airquality.app.models.AirQuality;
+
+@Repository
+public interface AirQualityRepository extends JpaRepository<AirQuality,Long> {
+    AirQuality findByCoordLatAndCoordLon(float coordLat, float coordLon);
 }
