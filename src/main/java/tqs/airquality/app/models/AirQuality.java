5f89d639a6dcf028d64c7660bdafe47576e6564d
Duarte Mortágua <duarte.ntm@ua.pt>
2021-04-24 02:00:31 +0100

    Added AirQuality entity to model (draft) ready to work with JPA if needed.

--- src/main/java/tqs/airquality/app/models/AirQuality.java
+++ src/main/java/tqs/airquality/app/models/AirQuality.java
@@ -1,4 +1,4 @@
-package tqs.airquality.app.data;
+package tqs.airquality.app.models;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,21 +19,45 @@ public class AirQuality {
     private Long id;
 
     @NotEmpty
-    private float coordLat;
+    private double coordLat;
     @NotEmpty
-    private float coordLon;
+    private double coordLon;
     @NotEmpty
     private Date dt;
     private int aqi;
 
-    private float co;
-    private float no;
-    private float no2;
-    private float o3;
-    private float so2;
-    private float pm2_5;
-    private float pm10;
-    private float nh3;
+    private double co;
+    private double no;
+    private double no2;
+    private double o3;
+    private double so2;
+    private double pm2_5;
+    private double pm10;
+    private double nh3;
 
-
+    public AirQuality(@NotEmpty double coordLat,
+                      @NotEmpty double coordLon,
+                      @NotEmpty Date dt,
+                      int aqi,
+                      double co,
+                      double no,
+                      double no2,
+                      double o3,
+                      double so2,
+                      double pm2_5,
+                      double pm10,
+                      double nh3) {
+        this.coordLat = coordLat;
+        this.coordLon = coordLon;
+        this.dt = dt;
+        this.aqi = aqi;
+        this.co = co;
+        this.no = no;
+        this.no2 = no2;
+        this.o3 = o3;
+        this.so2 = so2;
+        this.pm2_5 = pm2_5;
+        this.pm10 = pm10;
+        this.nh3 = nh3;
+    }
 }
