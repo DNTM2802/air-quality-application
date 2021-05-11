9dab4c74ad11a75bef8566bbd805b576a6e71320
Duarte Mortágua <duarte.ntm@ua.pt>
2021-05-11 19:56:45 +0100

    Added style to the web pages to improve UI.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,4 @@
-$('#datepickerStartDate').datepicker({
+<!-- $('#datepickerStartDate').datepicker({
     uiLibrary: 'bootstrap',
     format: 'dd/mm/yyyy'
 });
@@ -6,14 +6,5 @@ $('#datepickerStartDate').datepicker({
 $('#datepickerEndDate').datepicker({
     uiLibrary: 'bootstrap',
     format: 'dd/mm/yyyy'
-});
+}); -->
 
-$("#scope").change(function () {
-    if (this.value === "historical"){
-        document.getElementById("datepickerStartDate").removeAttribute("hidden");
-        document.getElementById("datepickerEndDate").removeAttribute("hidden");
-    } else {
-        document.getElementById("datepickerStartDate").setAttribute("hidden", true);
-        document.getElementById("datepickerEndDate").setAttribute("hidden", true);
-    }
-});
